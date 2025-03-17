690d319debe39c6c444554c11ba5294470395659
H4SSA4AN <hassan_touchent@outlook.com>
2025-03-17 18:45:11 +0000

    Created a main and cleaner. The cleaner class will clean the data, and the main will create a cleaner instance to test.

---
+++ cleaner.java
@@ -0,0 +1,10 @@
+public class cleaner {
+
+    // Contains methods that take the data from the excel sheet
+    // Randomises data, finds anomalies
+    // Need to turn every row into an array
+    // Whole dataset into a 2D array
+
+
+
+}
